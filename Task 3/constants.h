a63a62a6ef90cf6ebb53948d381ff0748dea9c10
Nikolay Kopytov <57916666+kopytovnn@users.noreply.github.com>
2023-11-13 18:55:52 +0300

    Add files via upload

---
+++ Task 3/constants.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef CONSTANTS_
+#define CONSTANTS_
+
+namespace CONSTANTS_JBS
+{
+    const float dt = 0.001f;
+    const float m = 300.0f;
+    const float lf = 0.721f;
+    const float lr = 0.823f;
+    const float Cm = 3600.0f;
+    const float Crr = 200.0f;
+    const float Cd = 1.53f;
+    const float Cbf = 5411.0f;
+    const float Cbr = 2650.0f;
+    const float Cx = 20000.0f;
+    const float Iz = 134.0f;
+}
+#endif
